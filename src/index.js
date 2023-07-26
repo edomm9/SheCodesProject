72a0b96760999163834e2ff3a0b541b6bbc0e36c
edomm9 <129413121+edomm9@users.noreply.github.com>
2023-07-27 00:19:48 +0300

    link added
    A link was added to access this repository

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ function search(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(weatherDisplay);
 }
-
 let form = document.querySelector("#form");
 form.addEventListener("click", search);
 
